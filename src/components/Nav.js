9fb255ba055e4682db08097c0f3f78420bab59d7
Szymon Dziewa <82220216+Szymofcion@users.noreply.github.com>
2023-07-05 17:38:28 +0200

    style: :art: repair mobile nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,43 +3,89 @@ import { useState, useEffect } from "react";
 import logo from "../assets/nike-logo.png";
 import CartButton from "./UI/CartButton";
 import "./Nav.scss";
+
 const Nav = ({ showCartHandler }) => {
   const [isScrolled, setIsScrolled] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
       setIsScrolled(window.scrollY > 0);
     };
 
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 780);
+      setIsMenuOpen(false); // Zamknij menu po zmianie szerokości ekranu
+    };
+
     window.addEventListener("scroll", handleScroll);
+    window.addEventListener("resize", handleResize);
+
+    // Inicjalizacja stanu na początku
+    handleResize();
 
     return () => {
       window.removeEventListener("scroll", handleScroll);
+      window.removeEventListener("resize", handleResize);
     };
   }, []);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <nav className={`navbar${isScrolled ? ' scrolled' : ''}`} data-bs-theme="dark">
-      <div className="container-fluid justify-content-between d-flex justify-content-center mt-4 ">
-        <img className="logo " src={logo} alt="logo"></img>
-        <div className="nav-link-container">
-          <a href="#home" className="nav-link mx-3 fs-5 ">
-            Home
-          </a>
-          <a href="#popularProducts" className="nav-link mx-3 fs-5 ">
-            Popular products
-          </a>
-          <a href="#aboutUs" className="nav-link mx-3 fs-5  ">
-            About
-          </a>
-          <a href="#contactUs" className="nav-link mx-3 fs-5  ">
-            Contact Us
-          </a>
-        </div>
+    <nav
+      className={`navbar${isScrolled ? " scrolled" : ""}`}
+      data-bs-theme="dark"
+    >
+      <div className="container-fluid justify-content-between d-flex justify-content-center mt-4">
+        <img className="logo" src={logo} alt="logo" />
+        {isMobile ? (
+          <button id="nav-icon1" className="menu-toggle" onClick={toggleMenu}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
+        ) : (
+          <div className={`nav-link-container${isMenuOpen ? " open" : ""}`}>
+            <a href="/" className="nav-link mx-3" onClick={toggleMenu}>
+              Home
+            </a>
+            <a href="/popular" className="nav-link mx-3" onClick={toggleMenu}>
+              Popular products
+            </a>
+            <a href="/about" className="nav-link mx-3" onClick={toggleMenu}>
+              About
+            </a>
+            <a href="/contact" className="nav-link mx-3" onClick={toggleMenu}>
+              Contact Us
+            </a>
+          </div>
+        )}
         <div className="d-flex align-items-center">
           <CartButton showCartHandler={showCartHandler} />
         </div>
       </div>
+      {isMobile && isMenuOpen && (
+        <div className="mobile-menu">
+          <a href="/" className="nav-link" onClick={toggleMenu}>
+            Home
+          </a>
+          <a href="/popular" className="nav-link" onClick={toggleMenu}>
+            Popular products
+          </a>
+          <a href="/about" className="nav-link" onClick={toggleMenu}>
+            About
+          </a>
+          <a href="/contact" className="nav-link" onClick={toggleMenu}>
+            Contact Us
+          </a>
+        </div>
+      )}
     </nav>
   );
 };
+
 export default Nav;
